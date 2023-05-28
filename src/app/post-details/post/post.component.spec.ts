1120a33e15b39a40cfd41d9575ff1bf5d95da59b
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-28 20:20:24 +0530

    fix: converted normal testing to testbed

--- src/app/post-details/post/post.component.spec.ts
+++ src/app/post-details/post/post.component.spec.ts
@@ -1,16 +1,35 @@
 import { PostComponent } from "./post.component"
 import { post } from '../../models/post.model'
 import { first } from "rxjs";
+import { TestBed, ComponentFixture } from "@angular/core/testing";
 describe('Post component', () => {
     let component: PostComponent;
+    let fixture: ComponentFixture<PostComponent>;
     let post: post = {
         id: 1,
         title: 'testing',
         body: 'this is testing of insurance',
     }
-    beforeEach(() => {
-        component = new PostComponent();
+    beforeAll(() => {
+        // component = new PostComponent();
+        TestBed.configureTestingModule({
+            declarations: [
+                PostComponent,
+            ]
+        })
+        fixture = TestBed.createComponent(PostComponent);
+        component = fixture.componentInstance;
         component.post = post;
+    })
+    it('should create post component using testbed', () => {
+        // TestBed.configureTestingModule({
+        //     declarations: [
+        //         PostComponent,
+        //     ]
+        // })
+        // const fixture = TestBed.createComponent(PostComponent); // fixture is known as class instance with the templete rendered
+        // const component = fixture.componentInstance;
+        expect(component).toBeDefined();
     })
     it('should raise an event when the delete post is clicked', (done) => {
         component.onDelete.pipe(
