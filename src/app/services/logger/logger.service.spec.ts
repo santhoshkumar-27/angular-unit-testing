2a8dcb427a7121fe8fdb9702ad2c214b93a75b83
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-30 05:10:10 +0530

    logger service uses testbed

--- src/app/services/logger/logger.service.spec.ts
+++ src/app/services/logger/logger.service.spec.ts
@@ -6,7 +6,11 @@ describe('LoggerService', () => {
   let service: LoggerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        LoggerService
+      ]
+    });
     service = TestBed.inject(LoggerService);
   });
 
