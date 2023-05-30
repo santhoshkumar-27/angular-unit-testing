f304e9d871ede4828a663e4aac320779be3bfdaa
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-30 06:01:47 +0530

    feat: included the testing module

--- src/app/services/post/post.service.spec.ts
+++ src/app/services/post/post.service.spec.ts
@@ -2,9 +2,10 @@ import { HttpClient } from "@angular/common/http";
 import { PostService } from "./post.service"
 import { post } from '../../models/post.model';
 import { of } from "rxjs";
+import { TestBed } from "@angular/core/testing";
 describe('post service', () => {
     let postService: PostService;
-    let mockPostService : jasmine.SpyObj<HttpClient>
+    let mockHttpClient : jasmine.SpyObj<HttpClient>
     const post: post[] = [
         {
             id: 1,
@@ -23,29 +24,40 @@ describe('post service', () => {
         },
     ]
     beforeAll(() => {
-        mockPostService = jasmine.createSpyObj('HttpClient', ['get', 'delete'])
-        postService = new PostService(mockPostService);
+        mockHttpClient = jasmine.createSpyObj('HttpClient', ['get', 'delete'])
+        TestBed.configureTestingModule({
+            providers: [
+                PostService,
+                {
+                    provide: HttpClient,
+                    useValue: mockHttpClient
+                }
+            ],
+
+        })
+        // postService = new PostService(mockHttpClient);
+        postService = TestBed.inject(PostService);
     })
     describe('get post method', () => {
         it('should return  expected post when get post is called', (done) => {
-            mockPostService.get.and.returnValue(of(post));
+            mockHttpClient.get.and.returnValue(of(post));
             postService.getPost().subscribe((response: post[]) => {
                 expect(response).toEqual(post);
                 done()
             });
-            expect(mockPostService.get).toHaveBeenCalled();
-            expect(mockPostService.get).toHaveBeenCalledTimes(1);
+            expect(mockHttpClient.get).toHaveBeenCalled();
+            expect(mockHttpClient.get).toHaveBeenCalledTimes(1);
         })
     })
 
     describe('delete post method', () => {
         it('should return method if delete action done perfectly', (done) => {
-            mockPostService.delete.and.returnValue(of(true));
+            mockHttpClient.delete.and.returnValue(of(true));
             postService.deletePost(post[0]).subscribe((res: any) => {
                 done();
             })
-            expect(mockPostService.delete).toHaveBeenCalled();
-            expect(mockPostService.delete).toHaveBeenCalledTimes(1);
+            expect(mockHttpClient.delete).toHaveBeenCalled();
+            expect(mockHttpClient.delete).toHaveBeenCalledTimes(1);
         })
     })
 })
