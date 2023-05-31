2acd5329a450d16e1257d38b7e85c99fc2eb8901
santhoshkumar <its.santhoshkumar@outlook.com>
2023-05-31 06:32:25 +0530

    feat: multiple request testing

--- src/app/services/post/httpclient.testing.spec.ts
+++ src/app/services/post/httpclient.testing.spec.ts
@@ -3,7 +3,7 @@ import { HttpClientTestingModule, HttpTestingController } from "@angular/common/
 import { TestBed } from "@angular/core/testing"
 
 const testUrl = '/data';
-interface  Data {
+interface Data {
     name: string;
 }
 describe('testing client module', () => {
@@ -30,5 +30,36 @@ describe('testing client module', () => {
         const request = httpTestingController.expectOne(testUrl);
         request.flush(testData)
         expect(request.request.method).toBe('GET');
+    });
+
+    it('should test multiple requests', () => {
+        let testData: Data[] = [{
+            name: 'Santhoshkumar'
+        }, {
+            name: 'kumar'
+        }];
+        httpClient.get<Data[]>(testUrl).subscribe(
+            (res: any) => {
+                expect(res.length).toBe(0);
+                // done();
+            }
+        );
+        httpClient.get<Data[]>(testUrl).subscribe(
+            (res: any) => {
+                expect(res.length).toBe(1);
+                // done();
+            }
+        );
+        httpClient.get<Data[]>(testUrl).subscribe(
+            (res: any) => {
+                expect(res.length).toBe(2);
+                // done();
+            }
+        );
+        const requests = httpTestingController.match(testUrl);
+        expect(requests.length).toBe(3);
+        requests[0].flush([]);
+        requests[1].flush([testData[0]]);
+        requests[2].flush(testData);
     })
 })
